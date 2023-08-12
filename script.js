d820da1ea09dcb8d83d265e8d8176d61ced23699
itsthekentinator <134373636+itsthekentinator@users.noreply.github.com>
2023-08-12 14:15:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+function goToMusic() {
+  // Replace this URL with the actual music recommendations URL
+  window.location.href = "https://www.example.com/kenny-music-recommendations";
+}
+
+function goToYoutube() {
+  // Replace this URL with the actual Youtube recommendations URL
+  window.location.href = "https://www.example.com/kenny-youtube-recommendations";
+}
